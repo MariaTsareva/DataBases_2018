f1d7f0acb795d76e9944af53a2764d33d02abaf6
MariaTsareva <ladymary9627@gmail.com>
2018-10-20 13:10:51 +0300

    First HW

--- Database/db.py
+++ Database/db.py
@@ -14,6 +14,11 @@ def days(dep_date, arr_date):
 
 
 @app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/newtrip')
 def trip():
     return render_template('trip.html')
 
@@ -23,36 +28,36 @@ def result():
     conn = sqlite3.connect('reise.db')
     c = conn.cursor()
     if request.method == 'POST':
-        # try:
-        emp = request.form['Employee']
-        print(type(emp))
-        dep_date = request.form['Departure date']
-        arr_date = request.form['Arrival date']
-        country = request.form['Country']
-        # with sqlite3.connect("reise.db") as conn:
-        #     c = conn.cursor()
-        # query =
-        # print(query)
-        emp_id = c.execute("SELECT id_employee FROM Employees WHERE name = ?", (emp,))
-        emp_id = emp_id.fetchone()[0]
-        print('test', emp_id)
-        country_id = c.execute("SELECT id_country FROM Countries WHERE country = ?", (country,)).fetchone()[0]
-        days_ = days(dep_date, arr_date)
-        rate = c.execute("SELECT rate FROM Countries WHERE country = ?", (country,)).fetchone()[0]
-        pay_day = c.execute("SELECT day_payment FROM Countries WHERE country = ?", (country,)).fetchone()[0]
-        sum_ = np.round(days_ * rate * pay_day, 2)
-        c.execute("INSERT INTO Trips (employee_id, dep_date ,arr_date, country_id, sum) VALUES (?,?,?,?,?)", \
-                  (emp_id, dep_date, arr_date, country_id, sum_))
-        #             c.execute("INSERT INTO Trips (sum) SELECT dep_date, arr_date, arr_date - dep_date FROM Trips  ")
-        conn.commit()
-        msg = "Record successfully added"
-            # return render_template("result.html", msg=msg)
+        try:
+            emp = request.form['Employee']
+            print(type(emp))
+            dep_date = request.form['Departure date']
+            arr_date = request.form['Arrival date']
+            country = request.form['Country']
+            # with sqlite3.connect("reise.db") as conn:
+            #     c = conn.cursor()
+            # query =
+            # print(query)
+            emp_id = c.execute("SELECT id_employee FROM Employees WHERE name = ?", (emp,))
+            emp_id = emp_id.fetchone()[0]
+            print('test', emp_id)
+            country_id = c.execute("SELECT id_country FROM Countries WHERE country = ?", (country,)).fetchone()[0]
+            days_ = days(dep_date, arr_date)
+            rate = c.execute("SELECT rate FROM Countries WHERE country = ?", (country,)).fetchone()[0]
+            pay_day = c.execute("SELECT day_payment FROM Countries WHERE country = ?", (country,)).fetchone()[0]
+            sum_ = np.round(days_ * rate * pay_day, 2)
+            c.execute("INSERT INTO Trips (employee_id, dep_date ,arr_date, country_id, sum) VALUES (?,?,?,?,?)", \
+                      (emp_id, dep_date, arr_date, country_id, sum_))
+            #             c.execute("INSERT INTO Trips (sum) SELECT dep_date, arr_date, arr_date - dep_date FROM Trips  ")
+            conn.commit()
+            msg = "Record successfully added"
+            return render_template("result.html", msg=msg)
             # return redirect(url_for('trip'), msg=msg, code=307)
-        # except:
-        #     conn.rollback()
-        #     msg = "error in insert operation"
-        # finally:
-        return render_template("result.html", msg=msg)
+        except:
+            conn.rollback()
+            msg = "error in insert operation"
+        finally:
+            return render_template("result.html", msg=msg)
 
 
 @app.route('/list')
